143df16089b360c75a12817429059b3a2973fa6f
Brandon Grenier <brandon.john.grenier@gmail.com>
2012-08-04 16:34:34 +1000

    Updates to the 7 card hand ranking generator.

--- lib/rora/utils/hand_ranking_generator.rb
+++ lib/rora/utils/hand_ranking_generator.rb
@@ -1,33 +1,7 @@
 class HandRankingGenerator
-  
+
   def initialize
     @scores = {}
-  end
-
-  def file_verification
-    five_card = Hash.new
-    CSV.foreach("lib/rora/5-card-hands.csv") do |row|
-      five_card[row[0].to_i] = [row[1],row[2],row[3],row[4]]
-    end
-
-    seven_card = Hash.new
-    CSV.foreach("lib/rora/7-card-hands.csv") do |row|
-      seven_card[row[0].to_i] = [row[1],row[2],row[3],row[4]]
-    end
-
-    count = 0
-    flush_count = 0
-    five_card.each_pair do |key,value|
-      if(!seven_card.has_key?(key))
-        if(!value[3].include?("Flush"))
-          puts "7 card hand ranking missing key #{key} { #{value[1]} => #{value[3]} }"
-          count = count + 1
-        else
-          flush_count = flush_count + 1
-        end
-      end
-    end
-    puts "Summary: #{count} 7-card hands are not included in 5-card ranking. #{flush_count} flush cards unnacounted for"
   end
 
   def generate_non_flush_rankings
@@ -38,14 +12,12 @@ class HandRankingGenerator
             Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).combination(1).each do |card_5|
               Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).remove(card_5).combination(1).each do |card_6|
                 Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).remove(card_5).remove(card_6).combination(1).each do |card_7|
-                  cards = card_1 + card_2 + card_3 + card_4 + card_5 + card_6 + card_7
+                  cards = (card_1 + card_2 + card_3 + card_4 + card_5 + card_6 + card_7).sort
                   key = get_key(cards)
                   hand = get_best_hand(cards)
-                  if(!hand.flush?)
-                    if(!@scores.has_key?(key))
-                      @scores[key] = hand.score
-                      puts "#{key},#{hand.score}"
-                    end
+                  if(!hand.flush? && !@scores.has_key?(key))
+                    @scores[key] = hand.score
+                    puts "#{hand.score},#{key},#{rank_string(hand.cards)},#{rank_string(cards)},#{hand.type.key},#{hand.name}"
                   end
                 end
               end
@@ -58,11 +30,13 @@ class HandRankingGenerator
 
   def generate_flush_rankings
     Suit.values().each do |suit|
-      # generate_hand_rankings(suit, 5, 2)
+      generate_hand_rankings(suit, 5, 2)
       generate_hand_rankings(suit, 6, 1)
-      # generate_hand_rankings(suit, 7, 0)
+      generate_hand_rankings(suit, 7, 0)
     end
   end
+
+  private
 
   def generate_hand_rankings(suit, i, j)
     collisions = 0
@@ -82,34 +56,19 @@ class HandRankingGenerator
         end
       end
     end
-    puts "found #{collisions} #{suit.value} collisions"
-  end
-
-  def rank_string(cards)
-    cards.inject('') { |string, card| string << card.key }
-  end
-
-  def get_key(cards)
-    key = cards.inject(1) {|product, card| product * card.rank.id }
   end
 
   def get_flush_key(cards, count)
     key = cards.inject(1) {|product, card| product * card.rank.id }
     cid = cards.inject(1) {|summand, card| summand * card.id }
-    "#{key}-#{cid}"    
-    
-#    suit_key = cards.inject(0) {|sum, card| sum + card.suit.id }
-#    rank_key = cards.inject(1) {|product, card| product * card.rank.id }
-#    key = rank_key + suit_key
-    #    if(count == 5)
-    #      return key * 71
-    #    end
-    #    if(count == 6)
-    #      return key * 73
-    #    end
-    #    if(count == 7)
-    #      return key * 79
-    #    end
+  end
+
+  def rank_string(cards)
+    cards.inject('') { |string, card| string << card.rank.key }
+  end
+
+  def get_key(cards)
+    key = cards.inject(1) {|product, card| product * card.rank.id }
   end
 
   def get_best_hand(cards)
--- test/rora/utils/hand_ranking_generator_test.rb
+++ test/rora/utils/hand_ranking_generator_test.rb
@@ -2,8 +2,12 @@ require File.expand_path("../../rora_test", File.dirname(__FILE__))
 
 class HandRankingGeneratorTest < ActiveSupport::TestCase
 
+  def setup
+    @generator = HandRankingGenerator.new
+  end
+
   test "should return equity calculations for a heads up game" do
-    #HandRankingGenerator.new.file_verification
+    # @generator.generate_non_flush_rankings
   end
 
 end
