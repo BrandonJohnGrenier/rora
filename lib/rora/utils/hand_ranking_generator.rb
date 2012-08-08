8dee8c4ddf37ce3b0f8680c85d345bd90b895728
Brandon Grenier <brandon.john.grenier@gmail.com>
2012-08-08 20:03:48 +1000

    Using a more efficient card enumeration technique in the hand ranking generator

--- lib/rora/utils/hand_ranking_generator.rb
+++ lib/rora/utils/hand_ranking_generator.rb
@@ -10,15 +10,16 @@ class HandRankingGenerator
 
   def generate_7_card_hand_rankings
     file = File.new("7CH.csv", "w")
+    cds = Deck.new.cards
 
-    Deck.new.combination(1).each do |card_1|
-      Deck.new.remove(card_1).combination(1).each do |card_2|
-        Deck.new.remove(card_1).remove(card_2).combination(1).each do |card_3|
-          Deck.new.remove(card_1).remove(card_2).remove(card_3).combination(1).each do |card_4|
-            Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).combination(1).each do |card_5|
-              Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).remove(card_5).combination(1).each do |card_6|
-                Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).remove(card_5).remove(card_6).combination(1).each do |card_7|
-                  cards = (card_1 + card_2 + card_3 + card_4 + card_5 + card_6 + card_7).sort
+    (0..45).each do |index_1|
+      ((index_1 + 1)..46).each do |index_2|
+        ((index_2 + 1)..47).each do |index_3|
+          ((index_3 + 1)..48).each do |index_4|
+            ((index_4 + 1)..49).each do |index_5|
+              ((index_5 + 1)..50).each do |index_6|
+                ((index_6 + 1)..51).each do |index_7|
+                  cards = [cds[index_1], cds[index_2], cds[index_3], cds[index_4], cds[index_5], cds[index_6], cds[index_7]].sort
                   hand = get_best_hand(cards)
                   key = get_key(cards, (hand.flush? ? true : false))
                   if(!@scores.has_key?(key))
@@ -27,7 +28,7 @@ class HandRankingGenerator
                   end
                   @count = @count + 1
                   if(@count % 50000 == 0)
-                    puts "#{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} :: #{count} rounds done, #{sprintf("%05.3f", (Float(@count)/Float(@total)) * 100.0)}% complete"
+                    puts "#{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} :: #{@count} rounds done, #{sprintf("%05.3f", (Float(@count)/Float(@total)) * 100.0)}% complete"
                   end
                 end
               end
