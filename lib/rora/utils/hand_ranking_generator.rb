0e5cd86a5936387590782951db6e95c5c19ecb4c
Brandon Grenier <brandon.john.grenier@gmail.com>
2012-08-08 20:23:52 +1000

    no message

--- lib/rora/utils/hand_ranking_generator.rb
+++ lib/rora/utils/hand_ranking_generator.rb
@@ -20,14 +20,14 @@ class HandRankingGenerator
                 Deck.new.remove(card_1).remove(card_2).remove(card_3).remove(card_4).remove(card_5).remove(card_6).combination(1).each do |card_7|
                   cards = (card_1 + card_2 + card_3 + card_4 + card_5 + card_6 + card_7).sort
                   hand = get_best_hand(cards)
-                  key = get_key(cards, (hand.flush? ? true : false))
+                  key = get_key(cards, hand)
                   if(!@scores.has_key?(key))
                     @scores[key] = hand.score
                     file.write("#{hand.score},#{key},#{rank_string(hand.cards)},#{rank_string(cards)},#{hand.type.key},#{hand.name}\n")
                   end
                   @count = @count + 1
                   if(@count % 50000 == 0)
-                    puts "#{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} :: #{count} rounds done, #{sprintf("%05.3f", (Float(@count)/Float(@total)) * 100.0)}% complete"
+                    puts "#{DateTime.now.strftime('%Y-%m-%d %H:%M:%S')} :: #{@count} rounds done, #{sprintf("%05.3f", (Float(@count)/Float(@total)) * 100.0)}% complete"
                   end
                 end
               end
@@ -42,8 +42,8 @@ class HandRankingGenerator
 
   private
 
-  def get_key(cards, flush)
-    cards.inject(1) {|product, card| product * (flush ? card.id : card.rank.id) }
+  def get_key(cards, hand)
+    cards.inject(1) {|product, card| product * (hand.flush? ? card.id : card.rank.id) }
   end
 
   def rank_string(cards)
